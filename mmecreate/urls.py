9c213b505713775db8da51099fab5052709592ce
carljame <carl@jovian34.com>
2020-10-31 16:38:56 -0400

    renamed admin and added mmestore to git

--- mmecreate/urls.py
+++ mmecreate/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+import os
+
+admin_word = os.environ.get('ADMIN_WORD')
+
 urlpatterns = [
     path('mmestore/', include('mmestore.urls')),
-    path('admin/', admin.site.urls),
+    path(f'{admin_word}/', admin.site.urls),
 ]
---
+++ mmestore/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Category(models.Model):
+    cat_name = models.CharField(max_length=200)
+    item_range_min = models.IntegerField()
+    item_range_max = models.IntegerField()
+
+    def __str__(self):
+        return self.cat_name
+
+
+class CraftItem(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    photo_front = models.URLField(max_length=200)
+    photo_back = models.URLField(max_length=200)
+    price = models.FloatField()
+    has_it_been_sold = models.BooleanField()
+    item_number = models.IntegerField(unique=True)
+    length = models.FloatField()
+    width = models.FloatField()
+    height = models.FloatField()
+    depth = models.FloatField()
+
+    def __str__(self):
+        return self.description
+
