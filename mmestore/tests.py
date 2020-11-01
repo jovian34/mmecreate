e3ff12ef1cfb5265725e0bf903c8691e4682adb9
carljame <carl@jovian34.com>
2020-11-01 16:29:56 -0500

    change secret key

--- mmestore/tests.py
+++ mmestore/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import CraftItem
+
+
+class CraftItemModelTests(TestCase):
+
+    def test_new_craft_item_has_unique_id(self):
+        item_one = CraftItem(category=1, item_number='0003', price=15)
+        item_two = CraftItem(category=2, item_number='0003', price=20)
+
+
+
+
