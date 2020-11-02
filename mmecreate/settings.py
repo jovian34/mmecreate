347e1d50ebd68099df912d673ccdfd9b2ebda39a
carljame <carl@jovian34.com>
2020-11-01 19:50:03 -0500

    turn debug off

--- mmecreate/settings.py
+++ mmecreate/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VALUE')
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['mmescreationsllc.com', 'www.mmescreationsllc.com', '167.71.188.32', 'localhost']
