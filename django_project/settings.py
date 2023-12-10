94fb57c19213448481a44c290ed0d77affed69ae
Carl James <carl@jovian34.com>
2023-12-10 14:28:05 -0500

    deleted old comments

--- django_project/settings.py
+++ django_project/settings.py
@@ -102,9 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "America/Indiana/Indianapolis"
@@ -114,10 +111,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = False
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
 
